9eba035ff862ab828b3d3f860c3d2278d1953906
kings <530675800@qq.com>
2017-10-23 18:30:36 +0800

    添加fetch语法糖

---
+++ Dev/src/assets/js/AFetch/index.js
@@ -0,0 +1,40 @@
+
+/*
+	AFetch
+	-------------------------------
+	对 fetch 添加语法糖
+
+	@url [string] 请求地址
+	@option [object] 请求方法
+	@resolve [callback] 成功回调方法
+	@reject  [callback] 失败请求方法
+*/
+export default function AFetch(url, option = false) {
+
+	return new Promise((resolve, reject) => {
+
+		if (!option) {
+			option = {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+					'Accept': 'application/json'
+				}
+			}
+		}
+
+		// options = { local: true } 强制使用本地数据
+		if (localStorage.SERVER_ENV === 'dev' || (option.local && option.local === true)) {
+			url = url.replace('.action', '.json')
+		}
+
+		fetch(url, option)
+			.then(res => res.json())
+			.then(res => {
+				if (resolve) resolve(res)
+			})
+			.catch(err => reject(err)) 
+		
+	})
+	
+}
