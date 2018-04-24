389441a60849a8dc35ba5196c79a7688ee610b13
ektx <530675800@qq.com>
2018-04-24 11:03:36 +0800

    优化在小屏幕上显示的效果

--- Dev/src/pages/home/index.vue
+++ Dev/src/pages/home/index.vue
@@ -10,10 +10,10 @@
 			<!-- 面包屑组件 自定义跳转方式 -->
 			<VBreadcrumb ref="vbreadcrumb" v-on:sendBreadCrumbEvt="emitBreadCrumbEvt"></VBreadcrumb>
 
-			<ul>
+			<ul class="file-list">
 				<li v-for="file in files" :key="file.file">
-					<svg v-if="!file.isDir" aria-hidden="true"  height="16" version="1.1" viewBox="0 0 12 16" width="14"><path d="M6 5H2V4h4v1zM2 8h7V7H2v1zm0 2h7V9H2v1zm0 2h7v-1H2v1zm10-7.5V14c0 .55-.45 1-1 1H1c-.55 0-1-.45-1-1V2c0-.55.45-1 1-1h7.5L12 4.5zM11 5L8 2H1v12h10V5z"></path></svg>
-					<svg v-else-if="file.isDir" aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M13 4H7V3c0-.66-.31-1-1-1H1c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V5c0-.55-.45-1-1-1zM6 4H1V3h5v1z"></path></svg>
+					<svg v-if="!file.isDir" aria-hidden="true"  version="1.1" viewBox="0 0 12 16"><path d="M6 5H2V4h4v1zM2 8h7V7H2v1zm0 2h7V9H2v1zm0 2h7v-1H2v1zm10-7.5V14c0 .55-.45 1-1 1H1c-.55 0-1-.45-1-1V2c0-.55.45-1 1-1h7.5L12 4.5zM11 5L8 2H1v12h10V5z"></path></svg>
+					<svg v-else-if="file.isDir" aria-hidden="true" version="1.1" viewBox="0 0 14 16"><path d="M13 4H7V3c0-.66-.31-1-1-1H1c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V5c0-.55-.45-1-1-1zM6 4H1V3h5v1z"></path></svg>
 
 					<a 
 						v-if="file.isDir" 
@@ -51,6 +51,6 @@
 	export default main
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 	@import './layout.scss'
 </style>
