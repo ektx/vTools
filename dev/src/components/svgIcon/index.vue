b86d3717d2541406f8735111e23452c645808316
ektx <530675800@qq.com>
2019-04-23 14:33:45 +0800

    SVGIcon

---
+++ dev/src/components/svgIcon/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <svg>
+        <use :xlink:href="iconName"/>
+    </svg>
+</template>
+
+<script>
+export default {
+    name: 'svg-icon-com',
+    props: {
+        icon: {
+            type: String,
+            default: ''
+        }
+    },
+    computed: {
+        iconName () {
+            return `#svg-icon-${this.icon}`
+        }
+    }
+}
+</script>
