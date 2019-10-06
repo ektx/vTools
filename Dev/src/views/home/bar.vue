1fabf990ef0d56146e56b3aa383dfd6d87902b5d
wuming <530675800@qq.com>
2019-10-06 11:47:58 +0800

    格式化文档

--- Dev/src/views/home/bar.vue
+++ Dev/src/views/home/bar.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="bar-mod">
-        <ul>
-            <li v-for="nav in navs" :key="nav.icon" @click="current = nav">
-                <svg-icon :class="['svg-icon', {hold: nav.hold}]" :icon="nav.icon"/>
-            </li>
-        </ul>
-    </div>
+  <div class="bar-mod">
+    <ul>
+      <li v-for="nav in navs" :key="nav.icon" @click="current = nav">
+        <svg-icon :class="['svg-icon', {hold: nav.hold}]" :icon="nav.icon" />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -15,72 +15,76 @@ export default {
     return {
       navs: [
         {
-          icon: 'directory',
+          icon: "directory",
           hold: true
         },
         {
-          icon: 'star',
+          icon: "star",
           hold: false
         },
         {
-          icon: 'qrcode',
+          icon: "qrcode",
           hold: false
         },
         {
-          icon: 'search',
+          icon: "search",
           hold: false
         }
       ],
       current: {}
-    }
+    };
   },
   watch: {
-    current (val, old) {
-      if (old) old.hold = false
-      val.hold = true
+    current(val, old) {
+      if (old) old.hold = false;
+      val.hold = true;
     }
   },
-  mounted () {
-    this.current = this.navs[0]
+  mounted() {
+    this.current = this.navs[0];
 
-    window.addEventListener('keydown', function (e) {
-      if(e.metaKey && e.keyCode == 'S'.charCodeAt(0)){
-        e.preventDefault();
-        alert("kl")
-      }
-    }, false)
+    window.addEventListener(
+      "keydown",
+      function(e) {
+        if (e.metaKey && e.keyCode == "S".charCodeAt(0)) {
+          e.preventDefault();
+          alert("kl");
+        }
+      },
+      false
+    );
   }
 };
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/css/mixin.scss';
+@import "@/assets/css/mixin.scss";
 
 .bar-mod {
-    width: 50px;
-    background-color: var(--mainBGColor);
-    @include BGTransition;
+  width: 50px;
+  background-color: var(--mainBGColor);
+  @include BGTransition;
 
-    ul {
-        margin: 30px 0 0;
+  ul {
+    margin: 30px 0 0;
 
-        li {
-            display: flex;
-            height: 50px;
-            justify-content: center;
-        }
+    li {
+      display: flex;
+      height: 50px;
+      justify-content: center;
     }
+  }
 
-    .svg-icon {
-        width: 24px;
-        fill: var(--waitColor);
-        cursor: pointer;
-        transition: fill .3s ease-in-out;
-    }
+  .svg-icon {
+    width: 24px;
+    fill: var(--waitColor);
+    cursor: pointer;
+    transition: fill 0.3s ease-in-out;
+  }
 
-    .svg-icon.hold,
-    .svg-icon:hover {
-        fill: var(--mainColor);
-    }
+  .svg-icon.hold,
+  .svg-icon:hover {
+    fill: var(--mainColor);
+  }
 }
 </style>
